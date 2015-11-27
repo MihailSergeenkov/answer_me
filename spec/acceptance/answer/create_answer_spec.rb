2b21dbc6ec89ca583e97b21dcdc40fe496accd45
Sergeenkov Mihail <sergeenkov.mihail@gmail.com>
2015-11-27 22:39:01 +0300

    Answers create with AJAX

--- spec/acceptance/answer/create_answer_spec.rb
+++ spec/acceptance/answer/create_answer_spec.rb
@@ -6,24 +6,24 @@ feature 'User create answer', %q{
 } do
 
   given(:user) { create(:user) }
-  given(:question) { create(:question) }
+  given!(:question) { create(:question) }
   given!(:answer) { create(:answer, question: question) }
-  given(:invalid_answer) { build(:invalid_answer, question: question) }
+  given!(:invalid_answer) { build(:invalid_answer, question: question) }
 
-  scenario 'User try to create answer the question' do
+  scenario 'User try to create answer the question', js: true do
     sign_in(user)
     visit question_path(question)
 
-    expect(current_path).to eq question_path(Question.last.id)
-    expect(page).to have_content answer.body
+    expect(current_path).to eq question_path(question)
+    within '.answers' do
+      expect(page).to have_content answer.body
+    end
   end
 
-  scenario 'User try to create answer the question with empty body' do
+  scenario 'User try to create answer the question with empty body', js: true do
     sign_in(user)
     visit question_path(question)
-    create_answer(invalid_answer)
 
-    expect(current_path).to eq question_answers_path(Question.last.id)
-    expect(page).to have_content 'Please, enter the correct data!'
+    expect(current_path).to eq question_path(question)
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   let(:user) { create(:user) }
-  let(:question) { create(:question, user: user) }  
+  let(:question) { create(:question, user: user) }
 
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
@@ -23,6 +23,10 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'assigns the requested question to @question' do
       expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns the new answer for question' do
+      expect(assigns(:answer)).to be_a_new(Answer)
     end
 
     it 'render show view' do
