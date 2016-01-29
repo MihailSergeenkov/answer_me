c245602dd84d9f4cf55e058f5dfac0812687feda
Sergeenkov Mihail <sergeenkov.mihail@gmail.com>
2016-01-29 19:41:01 +0300

    Add Default Mailer Host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.default_url_options = { host: '46.101.212.146' }
 end
+
+Rails.application.routes.default_url_options[:host] = '46.101.212.146'
